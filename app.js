c6707999a26eee7ac8bf6e0775955b5a3de93479
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-23 16:06:22 +0300

    data add

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// CORS
+
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+const app = express();
+// здесь мы указываем адрес нашего сервера
+const API_SERVICE_URL = "https://preeminent-taffy-d88f8d.netlify.app/";
+// прописываем следующую строку, если используется незашифрованное соединение
+// это серьезная брешь в безопасности, следует использовать только на этапе
+// разработки, и никогда в продакшене process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+// здесь мы указываем, какие заголовки нам нужно разрешить для использования app.use(cors({ exposedHeaders: '*' }));
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    ws: true,
+    logLevel: "debug",
+  })
+);
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy Server at ${HOST}:${PORT}`);
+});
+
+// >CORS
+
 fetch("https://preeminent-taffy-d88f8d.netlify.app/data.json")
   .then((response) => response.json())
   .then((item) => {
